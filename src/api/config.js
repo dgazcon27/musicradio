8cdf126e00af73ce4b02db84112de8889577f1f4
Daniel Gazcon <daniel.gazcon27@gmail.com>
2017-11-08 01:31:47 -0400

    initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appname: 'myradio',
+    apiKey: '5ec47958c8e76476865521a7f0cce3a4',
+    secret: '0085a5776ae8de0007478b1b239bfd6c',
+    author: 'dgazcon27'
+};
+
+// Application name	myradio
+// API key	5ec47958c8e76476865521a7f0cce3a4
+// Shared secret	0085a5776ae8de0007478b1b239bfd6c
+// Registered to	dgazcon27
